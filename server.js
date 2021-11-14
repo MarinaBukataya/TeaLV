32115a82735a3b08c52f3065f4c48e26b7d34ac0
Marina Bukataya <mbukataya@yandex.ru>
2021-11-14 18:56:02 +0200

    changed port

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
-app.use(fileUpload({
-    useTempFiles: true
-}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
-app.use('/user', require('./routers/userRouter'))
-app.use('/api', require('./routers/categoryRouter'))
-app.use('/api', require('./routers/productRouter'))
-app.use('/api', require('./routers/upload'))
+app.use("/user", require("./routers/userRouter"));
+app.use("/api", require("./routers/categoryRouter"));
+app.use("/api", require("./routers/productRouter"));
+app.use("/api", require("./routers/upload"));
 
-const URI = process.env.MONGODB_URI
+const URI = process.env.MONGODB_URI;
 
-mongoose.connect(URI, {
+mongoose.connect(
+  URI,
+  {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
+    useUnifiedTopology: true,
+  },
+  (err) => {
     if (err) throw err;
-    console.log('Connected to DB')
-})
+    console.log("Connected to DB");
+  }
+);
 
-app.listen(5000)
+var port = process.env.PORT || 5000;
+app.listen(port);
